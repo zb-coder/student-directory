ae7c6aca12245dcd6cfebb7a08599ac15df4b8bf
zainab <zainab_baruud@hotmail.co.uk>
2021-07-24 13:11:30 +0000

    (Step 8 exercise 8 and 9)

--- copy_of_directory.rb
+++ copy_of_directory.rb
@@ -8,9 +8,24 @@ def input_students
     # while the name is not empty, repeat this code
     while !name.empty? do
         puts "please enter the students cohort"
-        cohort = gets.chomp.to_sym
+        cohort = gets.chomp
+         if cohort == ""
+            cohort = "Not assigned"
+         else
+            cohort = cohort.to_sym
+         end
+         
+        puts "please enter a hobby of the student"
+        hobby = gets.chomp 
+
+        puts "please enter the country of birth of the student"
+        country_of_birth = gets.chomp
+
+        puts "please enter the height of the student"
+        height = gets.chomp
+        
         # add the student hash to the array
-        students << {name: name, cohort: cohort}
+        students << {name: name, cohort: cohort, hobby: :hobby, country_of_birth: :country_of_birth, height: :height}
         puts "Now we have #{students.count} students"
         # get another name from the user
         name = gets.chomp
@@ -30,38 +45,46 @@ def print_header
     puts "-------------"
 end
 
-def print(students)
-    a = 0
-    while a < students.length
-        puts "#{a + 1}. #{students[a][:name]} (#{students[a][:cohort]} cohort)"
-        a += 1
-        
-    grouped_by_cohort = Hash.new
-    
-    students.each do |student|
-        if grouped_by_cohort[student[:cohort]] == nil
-            grouped_by_cohort[student[:cohort]] = []
-        end
-    grouped_by_cohort[student[:cohort]].push(student[:name])
+def group_cohort(students)
+  groups = {}
+  students.each do |student|
+    cohort = student[:cohort].to_sym
+    if groups[cohort] == nil
+      groups[cohort] = []
     end
+    groups[cohort].push(student)
+  end
+  groups
+end
 
-grouped_by_cohort.each do |cohort, name|
-    puts "#{cohort} cohort"
-    puts name
-    puts "\n"
-    end
+
+def print(students)
+    students.each do |cohort, cohort_students|
+    puts "#{cohort.capitalize} Cohort"
+    cohort_students.each_with_index do |identity, index|
+        puts "#{index + 1}. #{identity[:name]}"
+        puts "-hobby: #{identity[:name]}"
+        puts "-country_of_birth: #{identity[:country_of_birth]}"
+        puts "-height: #{identity[:height]}"
     end
 end
 
 
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+    if students.count == 1
+        puts "Overall, we have 1 great student" 
+    else 
+        puts "Overall, we have #{students.count} great students"
+    end
+end
 end
 
 
 students = input_students
 #nothing happens until we call the methods
+length = print_students(students)
+grouped_students = group_cohort(length)
 print_header
-print(students)
+print(grouped_students)
 print_footer(students)
